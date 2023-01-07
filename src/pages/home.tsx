80b1ffe9ebca791867980cddb11733fd2b32bc20
Clement <clement.ramoslage@gmail.com>
2023-01-07 23:57:53 +0100

    feat: select workout day

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,26 +1,38 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useAuth } from '~/contexts/auth-provider'
 import { useWorkouts } from '~/hooks/workouts/use-workouts'
-
+import { formatDate } from '~/utils/common/date'
 import { WorkoutDescription } from '~/components/workout/workout-description'
 
 export default function Home() {
   const { signOut } = useAuth()
   const { data: workouts } = useWorkouts()
+  const [selected, setSelected] = useState<number | null>(null)
 
-  const [selectedWorkout, setSelectedWorkout] = useState({})
-
-  if (!workouts?.length) {
-    return <div>Aucun entrainement</div>
-  }
+  useEffect(() => {
+    if (workouts?.length) {
+      setSelected(workouts[0].id)
+    }
+  }, [workouts])
 
   return (
     <div>
-      <h2>HOME</h2>
+      <div className='flex justify-between'>
+        <h2>HOME</h2>
 
-      <WorkoutDescription workoutId={workouts[0].id} />
+        <button
+          onClick={signOut}
+          className='px-2 py-1 bg-teal-600 text-sm text-white rounded transition hover:bg-teal-500'
+        >
+          Logout
+        </button>
+      </div>
 
-      <button onClick={signOut}>Logout</button>
+      <select onChange={(v) => setSelected(Number(v.target.value))}>
+        {workouts?.map(w => <option key={w.id} value={w.id}>{formatDate(w.created_at)}</option>)}
+      </select>
+
+      {selected && <WorkoutDescription workoutId={selected} />}
     </div>
   )
 }
---
+++ src/utils/common/date.ts
@@ -0,0 +1,9 @@
+export const formatDate = (value: Date | string, options={
+  weekday: 'short',
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric'
+} as const): string => {
+  const date = value instanceof Date ? value : new Date(value)
+  return date.toLocaleString('fr-FR', options)
+}
