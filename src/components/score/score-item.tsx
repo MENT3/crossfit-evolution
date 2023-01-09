a38b184b19fb63cdf2acded6fb3cd2d78a4f6ea4
Clement <clement.ramoslage@gmail.com>
2023-01-09 14:03:07 +0100

    fix: remove border on last score item

--- src/components/score/score-item.tsx
+++ src/components/score/score-item.tsx
@@ -4,7 +4,7 @@ export const ScoreItem = (props: Database['public']['Tables']['score']['Row']) =
   const { name, value, type } = props
 
   return (
-    <li className="py-0.5 border-b">
+    <li className="py-0.5 border-b last:border-0">
       {name} 👉 {value} (type: {type})
     </li>
   )
